4606b6c2ab82a7d58e61c44fbbd2bfcd35bc72e7
Ron Metzger <Ron Metzger@192.168.0.68>
2017-03-30 19:41:20 +0200

    Aufgabe3 Done.

--- Aufg3/aufg3.js
+++ Aufg3/aufg3.js
@@ -2,19 +2,27 @@ document.addEventListener('DOMContentLoaded', function () {
     var Brett = 63;
     var Korn = 1;
     var SpaltenKastenzahl = 8;
+    var Zahl = 1;
     function Schachbrett() {
+        var Brett = document.getElementsByClassName("Brett")[0];
         for (var Spalte = 0; Spalte < SpaltenKastenzahl; Spalte++) {
             if (Spalte % 2 == 0) {
                 for (var Feld = 0; Feld < SpaltenKastenzahl; Feld++) {
                     if (Feld % 2 == 0) {
                         var elem = document.createElement('div');
                         console.log("erstes IF schwarz");
+                        Brett.appendChild(elem);
                         elem.className = "schwarz";
+                        elem.textContent = (Korn + "");
+                        Korn = Korn * 2;
                     }
                     else {
                         var elem = document.createElement('div');
                         console.log("erstes IF weis");
+                        Brett.appendChild(elem);
                         elem.className = "weis";
+                        elem.textContent = (Korn + "");
+                        Korn = Korn * 2;
                     }
                 }
             }
@@ -22,21 +30,25 @@ document.addEventListener('DOMContentLoaded', function () {
                 for (var Feld = 0; Feld < SpaltenKastenzahl; Feld++) {
                     if (Feld % 2 == 0) {
                         var elem = document.createElement('div');
-                        elem.className = "schwarz";
+                        Brett.appendChild(elem);
+                        elem.className = "weis";
+                        elem.textContent = (Korn + "");
+                        Korn = Korn * 2;
                         console.log("zweites IF schwarz");
                     }
                     else {
                         var elem = document.createElement('div');
                         console.log("zweites IF weis");
-                        elem.className = "weis";
+                        Brett.appendChild(elem);
+                        elem.className = "schwarz";
+                        elem.textContent = (Korn + "");
+                        Korn = Korn * 2;
                     }
                 }
             }
         }
         console.log("Ende");
     }
-    document.getElementById("Gruppe").addEventListener("click", function () {
-        Schachbrett();
-    });
+    Schachbrett();
 });
 //# sourceMappingURL=aufg3.js.map
--- Aufg3/aufg3.ts
+++ Aufg3/aufg3.ts
@@ -3,23 +3,28 @@ document.addEventListener('DOMContentLoaded', function() {
     let Brett: number = 63;
     let Korn: number = 1;
     let SpaltenKastenzahl: number = 8;
+    let Zahl: number=1;
     function Schachbrett(): void {
         
+        let Brett = document.getElementsByClassName("Brett")[0];
         for (let Spalte: number = 0; Spalte < SpaltenKastenzahl; Spalte++) {
-            if (Spalte % 2 == 0) {
-                
-                
+            if (Spalte % 2 == 0) {             
                 for (let Feld: number = 0; Feld < SpaltenKastenzahl; Feld++) {
                     if (Feld % 2 == 0) {
                         let elem: HTMLDivElement = document.createElement('div');
                         console.log("erstes IF schwarz");
+                        Brett.appendChild(elem);
                         elem.className = "schwarz";
-
+                         elem.textContent=(Korn+"");
+                        Korn=Korn*2;
                     }
                     else {
                         let elem: HTMLDivElement = document.createElement('div');
                            console.log("erstes IF weis");
+                         Brett.appendChild(elem);
                         elem.className = "weis";
+                        elem.textContent=(Korn+"");
+                        Korn=Korn*2;
                     }
                 }
             }
@@ -30,22 +35,28 @@ document.addEventListener('DOMContentLoaded', function() {
 
                     if (Feld % 2 == 0) {
                         let elem: HTMLDivElement = document.createElement('div');
-                        elem.className = "schwarz";
+                        Brett.appendChild(elem);
+                        elem.className = "weis";
+                       elem.textContent=(Korn+"");
+                        Korn=Korn*2;
+                        
                            console.log("zweites IF schwarz");
 
                     }
                     else {
                         let elem: HTMLDivElement = document.createElement('div');
                          console.log("zweites IF weis");
-                        elem.className = "weis";
+                         Brett.appendChild(elem);
+                        elem.className = "schwarz";
+                        elem.textContent=(Korn+"");
+                       Korn=Korn*2;
                     }
                 }
             }
         }
+        
     console.log("Ende");
-    }
-
-    document.getElementById("Gruppe").addEventListener("click", function() {
+        
+    } 
         Schachbrett();
-    })
 })
