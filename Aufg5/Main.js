ae2dae6df052c5c0d4576fb95f2cfa55cac340b7
Ron Metzger <Ron Metzger@RonLaptopLenovoY700.hfu-open.fu.hs-furtwangen.de>
2017-04-13 12:43:33 +0200

    Haus und Wiese gemacht

--- Aufg5/Main.js
+++ Aufg5/Main.js
@@ -9,26 +9,57 @@ var L4_Canvas;
         console.log(canvas);
         crc2 = canvas.getContext("2d");
         console.log(crc2);
-        crc2.fillStyle = "#0000ff";
-        crc2.fillRect(0, 0, canvas.width, canvas.height);
-        crc2.moveTo(0, 0);
-        crc2.lineTo(canvas.width, canvas.height);
-        crc2.stroke();
-        drawTriangle(200, 150, "#00ff00", "#ff0000");
+        drawWiese(0, 150, "#00FF00", "#00FF00");
+        drawHaus(100, 50, "#000000", "#000000");
+        drawDach(100, 50, "#000000", "#000000");
+        drawTuer(110, 100, "#5E3B24", "#5E3B24");
     }
-    function drawTriangle(_x, _y, _strokeColor, _fillColor) {
+    function drawWiese(_x, _y, _strokeColor, _fillColor) {
         crc2.beginPath();
         crc2.fillStyle = _fillColor;
         crc2.strokeStyle = _strokeColor;
-        crc2.moveTo(_x - 10, _y + 10);
-        crc2.lineTo(_x, _y - 10);
-        crc2.lineTo(_x + 10, _y + 10);
+        crc2.lineTo(_x + 400, _y + 0);
+        crc2.lineTo(_x + 400, _y + 150);
+        crc2.lineTo(_x + 0, _y + 150);
+        crc2.lineTo(_x + 0, _y + 0);
         crc2.closePath();
         crc2.fill();
         crc2.stroke();
-        //draw a triangle around the coordinates (_x, _y);
     }
-    function drawWiese(_x, _y, _strokeColor, _fillColor) {
+    function drawHaus(_x, _y, _strokeColor, _fillColor) {
+        crc2.beginPath();
+        crc2.fillStyle = _fillColor;
+        crc2.strokeStyle = _strokeColor;
+        crc2.lineTo(_x + 200, _y + 0);
+        crc2.lineTo(_x + 200, _y + 100);
+        crc2.lineTo(_x + 0, _y + 100);
+        crc2.lineTo(_x + 0, _y + 0);
+        crc2.closePath();
+        crc2.fill();
+        crc2.stroke();
+    }
+    function drawDach(_x, _y, _strokeColor, _fillColor) {
+        crc2.beginPath();
+        crc2.fillStyle = _fillColor;
+        crc2.strokeStyle = _strokeColor;
+        crc2.lineTo(_x + 200, _y + 0);
+        crc2.lineTo(_x + 100, _y - 50);
+        crc2.lineTo(_x + 0, _y + 0);
+        crc2.closePath();
+        crc2.fill();
+        crc2.stroke();
+    }
+    function drawTuer(_x, _y, _strokeColor, _fillColor) {
+        crc2.beginPath();
+        crc2.fillStyle = _fillColor;
+        crc2.strokeStyle = _strokeColor;
+        crc2.lineTo(_x + 25, _y + 0);
+        crc2.lineTo(_x + 25, _y + 50);
+        crc2.lineTo(_x + 0, _y + 50);
+        crc2.lineTo(_x + 0, _y + 0);
+        crc2.closePath();
+        crc2.fill();
+        crc2.stroke();
     }
 })(L4_Canvas || (L4_Canvas = {}));
 //# sourceMappingURL=Main.js.map
--- Aufg5/Main.ts
+++ Aufg5/Main.ts
@@ -11,35 +11,59 @@ namespace L4_Canvas {
 
         crc2 = canvas.getContext("2d");
         console.log(crc2);
-
-        crc2.fillStyle = "#0000ff";
-        crc2.fillRect(0, 0, canvas.width, canvas.height);
-
-        crc2.moveTo(0, 0);
-        crc2.lineTo(canvas.width, canvas.height);
-        crc2.stroke();
-
-        drawTriangle(200, 150, "#00ff00", "#ff0000");
-    }
-
-    function drawTriangle(_x: number, _y: number, _strokeColor: string, _fillColor: string): void {
-        crc2.beginPath();
-        crc2.fillStyle = _fillColor;
-        crc2.strokeStyle = _strokeColor;
-        crc2.moveTo(_x - 10, _y + 10);
-        crc2.lineTo(_x, _y - 10);
-        crc2.lineTo(_x + 10, _y + 10);
-        crc2.closePath();
-        crc2.fill();
-        crc2.stroke();
-        //draw a triangle around the coordinates (_x, _y);
+        drawWiese(0, 150, "#00FF00", "#00FF00");
+        drawHaus(100, 50, "#000000", "#000000");
+        drawDach(100, 50, "#000000", "#000000");
+        drawTuer(110, 100, "#5E3B24", "#5E3B24");
     }
 
 
     function drawWiese(_x: number, _y: number, _strokeColor: string, _fillColor: string): void {
-        
-        
-        }
+        crc2.beginPath();
+        crc2.fillStyle = _fillColor;
+        crc2.strokeStyle = _strokeColor;
+        crc2.lineTo(_x + 400, _y + 0);
+        crc2.lineTo(_x + 400, _y + 150);
+        crc2.lineTo(_x + 0, _y + 150);
+        crc2.lineTo(_x + 0, _y + 0);
+        crc2.closePath();
+        crc2.fill();
+        crc2.stroke();
+    }
+    function drawHaus(_x: number, _y: number, _strokeColor: string, _fillColor: string): void {
+        crc2.beginPath();
+        crc2.fillStyle = _fillColor;
+        crc2.strokeStyle = _strokeColor;
+        crc2.lineTo(_x + 200, _y + 0);
+        crc2.lineTo(_x + 200, _y + 100);
+        crc2.lineTo(_x + 0, _y + 100);
+        crc2.lineTo(_x + 0, _y + 0);
+        crc2.closePath();
+        crc2.fill();
+        crc2.stroke();
+    }
+    function drawDach(_x: number, _y: number, _strokeColor: string, _fillColor: string): void {
+        crc2.beginPath();
+        crc2.fillStyle = _fillColor;
+        crc2.strokeStyle = _strokeColor;
+        crc2.lineTo(_x + 200, _y + 0);
+        crc2.lineTo(_x + 100, _y - 50);
+        crc2.lineTo(_x + 0, _y + 0);
+        crc2.closePath();
+        crc2.fill();
+        crc2.stroke();
+    }
+    function drawTuer(_x: number, _y: number, _strokeColor: string, _fillColor: string): void {
+        crc2.beginPath();
+        crc2.fillStyle = _fillColor;
+        crc2.strokeStyle = _strokeColor;
+        crc2.lineTo(_x + 25, _y + 0);
+        crc2.lineTo(_x + 25, _y + 50);
+        crc2.lineTo(_x + 0, _y + 50);
+        crc2.lineTo(_x + 0, _y + 0);
+        crc2.closePath();
+        crc2.fill();
+        crc2.stroke();
 
-
+    }
 }
