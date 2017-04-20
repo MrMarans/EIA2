17b24eec3fa21724a461ace9a8543a0e5e551425
Ron Metzger <Ron Metzger@RonLaptopLenovoY700.hfu-open.fu.hs-furtwangen.de>
2017-04-20 12:21:01 +0200

    Calvas weiter gemacht

--- Aufg5/Main.js
+++ Aufg5/Main.js
@@ -9,19 +9,52 @@ var L4_Canvas;
         console.log(canvas);
         crc2 = canvas.getContext("2d");
         console.log(crc2);
-        drawWiese(0, 150, "#00FF00", "#00FF00");
-        drawHaus(100, 50, "#000000", "#000000");
-        drawDach(100, 50, "#000000", "#000000");
+        drawHimmel(0, 0, 0, "#BCEDDD", "#9CEDEC");
+        drawWiese2(0, 0, 180, "#98F2B7", "#98F2B7");
+        drawWiese(0, 0, 90, "#45F273", "#45F273");
+        drawWiesenschraege(0, 150, "#00F500", "#00F500");
+        drawHaus(50, 50, "#8F6552", "#8F6552");
+        drawDach(50, 50, "#705040", "#705040");
         drawTuer(110, 100, "#5E3B24", "#5E3B24");
+        drawFenster(150, 50, "#F0F0F0", "#F0F0F0");
     }
-    function drawWiese(_x, _y, _strokeColor, _fillColor) {
+    function drawHimmel(_x, _y, _z, _strokeColor, _fillColor) {
         crc2.beginPath();
         crc2.fillStyle = _fillColor;
         crc2.strokeStyle = _strokeColor;
-        crc2.lineTo(_x + 400, _y + 0);
+        crc2.ellipse(_x + 100, _y + 345, 1000, 1000, _z * Math.PI / 180, 0, 2 * Math.PI);
+        crc2.closePath();
+        crc2.fill();
+        crc2.stroke();
+    }
+    function drawWiese(_x, _y, _z, _strokeColor, _fillColor) {
+        crc2.beginPath();
+        crc2.fillStyle = _fillColor;
+        crc2.strokeStyle = _strokeColor;
+        //        crc2.lineTo(_x + 400, _y + 0);
+        //        crc2.lineTo(_x + 400, _y + 150);
+        //        crc2.lineTo(_x + 0, _y + 150);
+        //        crc2.lineTo(_x + 0, _y + 0);
+        crc2.ellipse(_x + 100, _y + 345, 200, 1000, _z * Math.PI / 180, 0, 2 * Math.PI);
+        crc2.closePath();
+        crc2.fill();
+        crc2.stroke();
+    }
+    function drawWiese2(_x, _y, _z, _strokeColor, _fillColor) {
+        crc2.beginPath();
+        crc2.fillStyle = _fillColor;
+        crc2.strokeStyle = _strokeColor;
+        crc2.ellipse(_x + 300, _y + 200, 200, 100, _z * Math.PI / 180, 0, 2 * Math.PI);
+        crc2.closePath();
+        crc2.fill();
+        crc2.stroke();
+    }
+    function drawWiesenschraege(_x, _y, _strokeColor, _fillColor) {
+        crc2.beginPath();
+        crc2.fillStyle = _fillColor;
+        crc2.strokeStyle = _strokeColor;
+        crc2.strokeStyle = _strokeColor;
         crc2.lineTo(_x + 400, _y + 150);
-        crc2.lineTo(_x + 0, _y + 150);
-        crc2.lineTo(_x + 0, _y + 0);
         crc2.closePath();
         crc2.fill();
         crc2.stroke();
@@ -56,6 +89,18 @@ var L4_Canvas;
         crc2.lineTo(_x + 25, _y + 0);
         crc2.lineTo(_x + 25, _y + 50);
         crc2.lineTo(_x + 0, _y + 50);
+        crc2.lineTo(_x + 0, _y + 0);
+        crc2.closePath();
+        crc2.fill();
+        crc2.stroke();
+    }
+    function drawFenster(_x, _y, _strokeColor, _fillColor) {
+        crc2.beginPath();
+        crc2.fillStyle = _fillColor;
+        crc2.strokeStyle = _strokeColor;
+        crc2.lineTo(_x + 25, _y + 0);
+        crc2.lineTo(_x + 25, _y + 25);
+        crc2.lineTo(_x + 0, _y + 25);
         crc2.lineTo(_x + 0, _y + 0);
         crc2.closePath();
         crc2.fill();
