0bc1d7ee049da26b8e40bb8b8dd9a47c1814791b
Ron Metzger <Ron Metzger@RonLaptopLenovoY700.hfu-open.fu.hs-furtwangen.de>
2017-05-04 12:38:49 +0200

    first push aufg.6/7/?

--- Aufg7/ts.js
+++ Aufg7/ts.js
@@ -21,10 +21,51 @@ var StudiVZ;
         }
     }
     function saveData(_input) {
-        return "Hier fehlt noch der richtige Code...";
-    }
-    function queryData(_matrikel) {
-        return "Hier fehlt noch der richtige Code...";
+        var List = _input.split(", ");
+        var i = {
+            studentnumber: parseInt(List[0]),
+            surname: List[1],
+            name: List[2],
+            age: parseInt(List[3]),
+            sex: parseInt(List[4]),
+            comment: List[5],
+        };
+        var sexNumb = List[4];
+        students.push(s);
+        var sex;
+        switch (sexNumb) {
+            case 0:
+                {
+                    sex == Male;
+                    break;
+                }
+            case 1:
+                {
+                    sex == Female;
+                    break;
+                }
+            case 2:
+                {
+                    sex == apache_helicopter;
+                    break;
+                }
+        }
+        return "Daten gespeichert\nMatrikelnr: " + s.matrikelnr + "\nName: " + s.surname + "\nVorname: " + s.name + "\nAlter: " + s.age + "\nGeschlecht: " + sex + "\nKommentar: " + s.comment;
     }
 })(StudiVZ || (StudiVZ = {}));
+function queryData(_matrikel) {
+    for (var i = 0; i < List.length; i++) {
+        if (students[i].matrikelnr == _matrikel) {
+            var sex = void 0;
+            if (students[i].sex == true) {
+                sex = "m";
+            }
+            else {
+                sex = "w";
+            }
+            return "Matrikelnr: " + students[i].matrikelnr + "\nName: " + students[i].surname + "\nVorname: " + students[i].name + "\nAlter: " + students[i].age + "\nGeschlecht: " + sex + "\nKommentar: " + students[i].comment;
+        }
+        else { }
+    }
+}
 //# sourceMappingURL=ts.js.map
--- Aufg7/ts.ts
+++ Aufg7/ts.ts
@@ -26,9 +26,58 @@ namespace StudiVZ {
     }
 
     function saveData(_input: string): string {
-        return "Hier fehlt noch der richtige Code...";
+        let List: string[] = _input.split(", ");
+        let i: StudentData = {
+            studentnumber: parseInt(List[0]),
+            surname: List[1],
+            name: List[2],
+            age: parseInt(List[3]),
+            sex: parseInt(List[4]),
+            comment: List[5],
+
+        };
+        let sexNumb: number = List[4];
+        students.push(s);
+
+        let sex: String;
+        switch (sexNumb) {
+            case 0:
+                {
+                    sex == Male;
+                    break;
+                }
+            case 1:
+                {
+                    sex == Female;
+                    break;
+                }
+            case 2:
+                {
+                    sex == apache_helicopter;
+                    break;
+                }
+
+        }
+
+        return "Daten gespeichert\nMatrikelnr: " + s.matrikelnr + "\nName: " + s.surname + "\nVorname: " + s.name + "\nAlter: " + s.age + "\nGeschlecht: " + sex + "\nKommentar: " + s.comment;
     }
-    function queryData(_matrikel: number): string {
-        return "Hier fehlt noch der richtige Code...";
+}
+function queryData(_matrikel: number): string {
+
+    for (let i: number = 0; i < List.length; i++) {
+        if (students[i].matrikelnr == _matrikel) {
+            let sex: string;
+
+            if (students[i].sex == true) {
+                sex = "m";
+            }
+            else {
+                sex = "w";
+            }
+
+            return "Matrikelnr: " + students[i].matrikelnr + "\nName: " + students[i].surname + "\nVorname: " + students[i].name + "\nAlter: " + students[i].age + "\nGeschlecht: " + sex + "\nKommentar: " + students[i].comment;
+        }
+        else { }
     }
+}
 }
