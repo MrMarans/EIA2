206aa57f2f9def7313cf6d5df592f05073a68c41
MrMarans <ron_marvin@yahoo.de>
2017-05-20 16:14:14 +0200

    Wie bekomme ich die Infos der Blume...mmmmh

--- Aufg8/Honigbiene.js
+++ Aufg8/Honigbiene.js
@@ -7,9 +7,19 @@ var Aufg8_Main;
 (function (Aufg8_Main) {
     var HoneyBee = (function (_super) {
         __extends(HoneyBee, _super);
-        function HoneyBee(_x, _y) {
-            _super.call(this, _x, _y);
+        function HoneyBee(_x, _y, _leftpush, _rightpush, _color, _hungry) {
+            _super.call(this, _x, _y, _leftpush, _rightpush, _color);
+            this.hungry = _hungry;
         }
+        HoneyBee.prototype.HoneybeeUpdate = function () {
+            this.draw();
+            this.update();
+        };
+        HoneyBee.prototype.Flowermove = function () {
+            if (this.hungry = true) {
+                Aufg8_Main.flowerSettings[Math.random() * 100] = y;
+            }
+        };
         return HoneyBee;
     }(Aufg8_Main.bee));
     Aufg8_Main.HoneyBee = HoneyBee;
--- Aufg8/Honigbiene.ts
+++ Aufg8/Honigbiene.ts
@@ -3,8 +3,21 @@ namespace Aufg8_Main {
 
 
     export class HoneyBee extends bee {
-        constructor(_x: number, _y: number) {
-            super(_x, _y);
+        hungry: boolean;
+        constructor(_x: number, _y: number, _leftpush: number, _rightpush: number, _color: string, _hungry: boolean) {
+            super(_x, _y, _leftpush, _rightpush, _color);
+            this.hungry = _hungry
         }
+        HoneybeeUpdate(): void {
+            this.draw();
+            this.update();
+        }
+        Flowermove(): void {
+            if (this.hungry = true) {
+                flowerSettings[Math.random() * 100] = y;
+            }
+        }
+
+
     }
 }
