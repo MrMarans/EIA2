f531d66e2514b0241bb42149d4aabcd8d64daf3e
MrMarans <ron_marvin@yahoo.de>
2017-06-18 17:15:31 +0200

    Fragen Update

--- Aufg9/main.js
+++ Aufg9/main.js
@@ -6,7 +6,7 @@ Name: Metzger, Ron
 Matrikel: 254878
 Datum: 11.06.2017
     
-Hier0mit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
 */
     window.addEventListener("load", start);
     var sorts = ["Erdbeer", "Himbeer", "Erdbeere"];
